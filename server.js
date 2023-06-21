b6c77a44ab4999f49dac6fc7386bff95ed986ceb
Patrick Lin <linpatrick11@gmail.com>
2023-06-21 15:34:35 -0400

    add endpoint choose

--- server.js
+++ server.js
@@ -6,6 +6,23 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/twice.html');
 });
 
+const members = [];
+members.push(
+  'tzuyu',
+  'mina',
+  'jihyo',
+  'sana',
+  'dahyun',
+  'chaeyoung',
+  'nayeon',
+  'jeongyoung',
+  'momo'
+);
+
+app.get('/choose', (req, res) => {
+  res.send(members[Math.floor(Math.random() * members.length)]);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
