41f07cc64f6d991e2877c4619d1df0af28542545
Patrick Lin <linpatrick11@gmail.com>
2023-06-21 12:38:03 -0400

    fix path

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ app.get('/', (req, res) => {
 });
 
 app.get('/tzuyu', function (req, res) {
-  res.sendFile('E:\\Patrick\\c1\\projects\\aws-week2\\tzuyu.html');
+  res.sendFile(__dirname + '\\tzuyu.html');
 });
 app.get('/jihyo', function (req, res) {
-  res.sendFile('E:\\Patrick\\c1\\projects\\aws-week2\\jihyo.html');
+  res.sendFile(__dirname + '\\jihyo.html');
 });
 
 app.listen(port, () => {
